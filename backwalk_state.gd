c20a62edf4cde00dd73f4047126118b4a5ed70b8
MichaelRatel <124313125+MichaelRatel@users.noreply.github.com>
2023-09-16 21:33:38 -0400

    Fixed typo. fixed #2

--- backwalk_state.gd
+++ backwalk_state.gd
@@ -6,6 +6,8 @@ var friction = 0.003
 
 func _physics_process(_delta):
 	persistent_state.player.velocity.x *= friction
+	if (!Input.is_anything_pressed()):
+		change_state.call("idle")
 
 # while we are moving forwards, set our velocity back to 0, then change to the walking state
 func move_forward():
@@ -14,5 +16,4 @@ func move_forward():
 
 # While we are moving backwards, keep moving backwards.
 func move_backwards():
-	print("continuing to move back")
 	persistent_state.player.velocity += Vector2(1, 0) * persistent_state.backwardWalkSpeed * (-1 * persistent_state.direction)
